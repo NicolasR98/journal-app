9fccecfb6974e165c167b6f344c89a6b17467570
Nicolas Rios <nicolas.rios@icareweb.com>
2022-01-29 21:20:36 +0100

    Add styles on `<RegisterScreen />`

--- src/components/auth/RegisterScreen.js
+++ src/components/auth/RegisterScreen.js
@@ -1,9 +1,58 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 
 export const RegisterScreen = () => {
     return (
-        <div>
-            <h1>RegisterScreen</h1>
-        </div>
+        <>
+            <h3 className="auth__title">Register</h3>
+
+            <form>
+                <input
+                    type="text"
+                    placeholder="Name"
+                    name="name"
+                    className="auth__input"
+                    autoComplete="off"
+                />
+
+                <input
+                    type="text"
+                    placeholder="Email"
+                    name="email"
+                    className="auth__input"
+                    autoComplete="off"
+                />
+
+                <input
+                    type="password"
+                    placeholder="Password"
+                    name="password"
+                    className="auth__input"
+                    autoComplete="off"
+                />
+
+                <input
+                    type="password"
+                    placeholder="Confirm password"
+                    name="passwordConfirm"
+                    className="auth__input"
+                    autoComplete="off"
+                />
+
+                <button
+                    className="btn btn-block btn-primary mb-5"
+                    type="submit"
+                >
+                    Register
+                </button>
+
+                <Link
+                    className="link"
+                    to="/auth/login"
+                >
+                    Already registered?
+                </Link>
+            </form>
+        </>
     )
 }

f7e1d21883662eb4c5e91b15cb1f327500b1220f
Nicolas Rios <nicolas.rios@icareweb.com>
2022-01-30 22:08:38 +0100

    Add first `auth` dispatch

--- src/components/auth/LoginScreen.js
+++ src/components/auth/LoginScreen.js
@@ -1,18 +1,39 @@
 import React from "react"
+import { useDispatch } from "react-redux";
+
 import { Link } from "react-router-dom";
+import { login } from "../../actions/auth";
+import { useForm } from "../../hooks/useForm";
+
+const initialState = {
+    email: 'example@mail.com',
+    password: '12345',
+};
 
 export const LoginScreen = () => {
+    const dispatch = useDispatch()
+
+    const [formValues, handleInputChange] = useForm(initialState)
+    const { email, password } = formValues;
+
+    const handleLogin = (e) => {
+        e.preventDefault();
+        dispatch(login(12345, 'nico'))
+    }
+
     return (
         <>
             <h3 className="auth__title">Login</h3>
 
-            <form>
+            <form onSubmit={handleLogin}>
                 <input
                     type="text"
                     placeholder="Email"
                     name="email"
                     className="auth__input"
                     autoComplete="off"
+                    value={email}
+                    onChange={handleInputChange}
                 />
 
                 <input
@@ -21,6 +42,8 @@ export const LoginScreen = () => {
                     name="password"
                     className="auth__input"
                     autoComplete="off"
+                    value={password}
+                    onChange={handleInputChange}
                 />
 
                 <button
@@ -46,7 +69,7 @@ export const LoginScreen = () => {
                     </div>
                 </div>
 
-                <Link 
+                <Link
                     className="link"
                     to="/auth/register"
                 >

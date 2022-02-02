a7ed71a959f335b4ecd451d1cd89110653854c57
Nicolas Rios <nicolas.rios@icareweb.com>
2022-02-02 19:56:25 +0100

    Add basic validations on `<RegisterScreen />`

--- src/components/auth/RegisterScreen.js
+++ src/components/auth/RegisterScreen.js
@@ -1,18 +1,71 @@
 import React from 'react'
+import { useDispatch, useSelector } from 'react-redux'
+
 import { Link } from 'react-router-dom'
 
+import isEmail from 'validator/lib/isEmail'
+
+import { removeError, setError } from '../../actions/ui'
+import { useForm } from '../../hooks/useForm'
+
+const initialState = {
+    name: 'Hernando',
+    email: 'example@mail.com',
+    password: '12345',
+    passwordConfirm: '12345',
+};
+
 export const RegisterScreen = () => {
+    const dispatch = useDispatch();
+    const { msgError } = useSelector(state => state?.ui);
+
+    const [formValues, handleInputChange] = useForm(initialState);
+    const { name, email, password, passwordConfirm } = formValues;
+
+    const handleRegister = (e) => {
+        e.preventDefault();
+        if (isFormValid()) {
+            console.log(formValues);
+        }
+    };
+
+    const isFormValid = () => {
+        if (name.trim().length === 0) {
+            dispatch(setError('Name required'));
+            return false;
+        } else if (!isEmail(email)) {
+            dispatch(setError('Email is not valid'));
+            return false;
+        } else if (password !== passwordConfirm || password.length < 5) {
+            dispatch(setError('Password should be at least 6 characters long'));
+            return false;
+        } else {
+            dispatch(removeError());
+            return true;
+        };
+    };
+
     return (
         <>
             <h3 className="auth__title">Register</h3>
 
-            <form>
+            <form onSubmit={handleRegister}>
+                {
+                    msgError && (
+                        <div className='auth__alert-error'>
+                            {msgError}
+                        </div>
+
+                    )
+                }
                 <input
                     type="text"
                     placeholder="Name"
                     name="name"
                     className="auth__input"
                     autoComplete="off"
+                    onChange={handleInputChange}
+                    value={name}
                 />
 
                 <input
@@ -21,6 +74,8 @@ export const RegisterScreen = () => {
                     name="email"
                     className="auth__input"
                     autoComplete="off"
+                    onChange={handleInputChange}
+                    value={email}
                 />
 
                 <input
@@ -29,6 +84,8 @@ export const RegisterScreen = () => {
                     name="password"
                     className="auth__input"
                     autoComplete="off"
+                    onChange={handleInputChange}
+                    value={password}
                 />
 
                 <input
@@ -37,6 +94,8 @@ export const RegisterScreen = () => {
                     name="passwordConfirm"
                     className="auth__input"
                     autoComplete="off"
+                    onChange={handleInputChange}
+                    value={passwordConfirm}
                 />
 
                 <button
--- src/store/store.js
+++ src/store/store.js
@@ -2,10 +2,12 @@ import { createStore, combineReducers, applyMiddleware } from 'redux';
 import thunk from 'redux-thunk';
 import { composeWithDevTools } from 'redux-devtools-extension';
 import { authReducer } from '../reducers/authReducer';
+import { uiReducer } from '../reducers/uiReducer';
 
 
 const reducers = combineReducers({
-    auth: authReducer
+    auth: authReducer,
+    ui: uiReducer,
 });
 
 export const store = createStore(
